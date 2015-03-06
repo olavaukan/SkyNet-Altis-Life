18635e0631f15cbd94778ecf76623d9c304b4ea5
Olav Aukan <olav.aukan@gmail.com>
2015-03-06 02:18:19 +0100

    Maybe it's missing vc_redist files?

--- buildProject.ps1
+++ buildProject.ps1
@@ -1,2 +1,12 @@
-BuildTools\MakePbo.exe -RT -L -P extDB-Build\Altis_Life.Altis
-BuildTools\MakePbo.exe -RT -L -P extDB-Build\life_server
+$OutputVariable = (& BuildTools\MakePbo.exe -RT -L -P extDB-Build\Altis_Life.Altis 2>&1)
+Write-Host "Output1Begin"
+Write-Host $OutputVariable
+Write-Host "Output1End"
+
+Write-Host "Output2Begin"
+$OutputVariable2 = (& BuildTools\MakePbo.exe -RT -L -P extDB-Build\life_server 2>&1)
+Write-Host $OutputVariable2
+Write-Host "Output2End"
+
+#BuildTools\MakePbo.exe -RT -L -P extDB-Build\Altis_Life.Altis
+#BuildTools\MakePbo.exe -RT -L -P extDB-Build\life_server
