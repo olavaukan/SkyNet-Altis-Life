bc6323ec18492ecb734bf76fcc39113b52b824c4
Olav Aukan <olav.aukan@gmail.com>
2015-03-06 03:45:40 +0100

    "Cleaned up" the build script a bit

--- buildProject.ps1
+++ buildProject.ps1
@@ -1,12 +1,25 @@
-#$OutputVariable = (& BuildTools\MakePbo.exe -RT -L -P extDB-BuildAltis_Life.Altis 2>&1)
-#Write-Host "Output1Begin"
-#Write-Host $OutputVariable
-#Write-Host "Output1End"
-
-#Write-Host "Output2Begin"
-#$OutputVariable2 = (& BuildTools\MakePbo.exe -RT -L -P extDB-Build\life_server 2>&1)
-#Write-Host $OutputVariable2
-#Write-Host "Output2End"
+Write-Host
+Write-Host "################################################################################################"
+Write-Host "##              Building 'Altis_Life.pbo' using MakePbo.exe...                                ##"
+Write-Host "################################################################################################"
+Write-Host
 
 BuildTools\MakePbo.exe -RT -L -P extDB-Build\Altis_Life.Altis
+
+Write-Host
+Write-Host "################################################################################################"
+Write-Host "##              Finished building 'Altis_Life.pbo'                                            ##"
+Write-Host "################################################################################################"
+Write-Host
+Write-Host "################################################################################################"
+Write-Host "##              Building 'life_server.pbo' using MakePbo.exe...                               ##"
+Write-Host "################################################################################################"
+Write-Host
+
 BuildTools\MakePbo.exe -RT -L -P extDB-Build\life_server
+
+Write-Host
+Write-Host "################################################################################################"
+Write-Host "##              Finished building 'life_server.pbo'                                           ##"
+Write-Host "################################################################################################"
+Write-Host
