a6d93fa96059417b24b9b2ee17ca3a6362264323
Olav Aukan <olav.aukan@gmail.com>
2015-03-06 04:14:58 +0100

    Typo...

--- buildProject.ps1
+++ buildProject.ps1
@@ -1,6 +1,6 @@
 Write-Host
 Write-Host "################################################################################################"
-Write-Host "##              Building 'Altis_Life.pbo' using MakePbo.exe...                                ##"
+Write-Host "##              Building 'Altis_Life.Altis.pbo' using MakePbo.exe...                          ##"
 Write-Host "################################################################################################"
 Write-Host
 
@@ -8,7 +8,7 @@ BuildTools\MakePbo.exe -RT -L -P extDB-Build\Altis_Life.Altis
 
 Write-Host
 Write-Host "################################################################################################"
-Write-Host "##              Finished building 'Altis_Life.pbo'                                            ##"
+Write-Host "##              Finished building 'Altis_Life.Altis.pbo'                                      ##"
 Write-Host "################################################################################################"
 Write-Host
 Write-Host "################################################################################################"
