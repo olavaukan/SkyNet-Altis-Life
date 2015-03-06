97f2d10991bfc248c79f3b135df2cb2c8f85277c
Olav Aukan <olav.aukan@gmail.com>
2015-03-06 02:45:12 +0100

    Tryp the "simple approach" again

--- buildProject.ps1
+++ buildProject.ps1
@@ -1,12 +1,12 @@
-$OutputVariable = (& BuildTools\MakePbo.exe -RT -L -P extDB-Build\Altis_Life.Altis 2>&1)
-Write-Host "Output1Begin"
-Write-Host $OutputVariable
-Write-Host "Output1End"
+#$OutputVariable = (& BuildTools\MakePbo.exe -RT -L -P extDB-BuildAltis_Life.Altis 2>&1)
+#Write-Host "Output1Begin"
+#Write-Host $OutputVariable
+#Write-Host "Output1End"
 
-Write-Host "Output2Begin"
-$OutputVariable2 = (& BuildTools\MakePbo.exe -RT -L -P extDB-Build\life_server 2>&1)
-Write-Host $OutputVariable2
-Write-Host "Output2End"
+#Write-Host "Output2Begin"
+#$OutputVariable2 = (& BuildTools\MakePbo.exe -RT -L -P extDB-Build\life_server 2>&1)
+#Write-Host $OutputVariable2
+#Write-Host "Output2End"
 
-#BuildTools\MakePbo.exe -RT -L -P extDB-Build\Altis_Life.Altis
-#BuildTools\MakePbo.exe -RT -L -P extDB-Build\life_server
+BuildTools\MakePbo.exe -RT -L -P extDB-Build\Altis_Life.Altis
+BuildTools\MakePbo.exe -RT -L -P extDB-Build\life_server
